c769d90321716f224e9849f641e388a99473d1a0
Kelsea Fields <kelsea.fields@yahoo.com>
2020-12-17 16:59:10 -0800

    seed data

---
+++ db/seed.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name) VALUES ('Barely-A-Burger');
+INSERT INTO burgers (burger_name) VALUES ('Mediocre-Burger');
+INSERT INTO burgers (burger_name) VALUES ('Son-Of-A-Burger');
+INSERT INTO burgers (burger_name) VALUES ('Father-Of-All-Burgers');
