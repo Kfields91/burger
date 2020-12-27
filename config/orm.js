a5134706625c097797482320e42941030f423eab
Kelsea Fields <kelsea.fields@yahoo.com>
2020-12-26 19:52:20 -0800

    working on get method

--- config/orm.js
+++ config/orm.js
@@ -1,15 +1,17 @@
 var connection = require("../config/connection.js");
 
-function selectAll(){
+var orm = {
+  selectAll: (tableName, cb) => {
+    const queryString = "SELECT * FROM " + tableName + ";";
+    connection.query(queryString, function (err, result) {
+      if (err) throw err;
+      //   currently only console logging the result. Is this why it's not showing up in the browser?
+      cb(result);
+    });
+  },
+  //   insertOne: () => {},
 
+  //   updateOne: () => {},
 };
 
-function insertOne(){
-
-};
-
-function updateOne(){
-
-};
-
-module.exports = orm; 
+module.exports = orm;
--- controllers/burgers_controllers.js
+++ controllers/burgers_controllers.js
@@ -3,8 +3,14 @@ const burgers = require("../models/burger");
 
 var router = express.Router();
 
-
-
-
+router.get("/", function (req, res) {
+  burgers.selectAll(function (data) {
+    var burgerObject = {
+      burgers: data,
+    };
+    res.render("index", burgerObject);
+    console.log(burgerObject);
+  });
+});
 
 module.exports = router;
