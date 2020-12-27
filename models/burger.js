8dd2bfc92611cfaff69be262f6a8cf3c1f4ddc24
Kelsea Fields <kelsea.fields@yahoo.com>
2020-12-26 19:53:06 -0800

    using orm

--- models/burger.js
+++ models/burger.js
@@ -1,7 +1,23 @@
 const orm = require("../config/orm");
 
+var burgers = {
+  selectAll: function (cb) {
+    orm.selectAll("burgers", function (res) {
+      cb(res);
+    });
+  },
 
+  insertOne: function (cols, vals, cb) {
+    orm.insertOne("burgers", cols, vals, function (res) {
+      cb(res);
+    });
+  },
 
-
+  updateOne: function (objVolVals, condition, cb) {
+    orm.insertOne("burger", objVolVals, condition, function (res) {
+      cb(res);
+    });
+  },
+};
 
 module.exports = burgers;
