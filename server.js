1f78a5d8aa51803252608a6516f531ced62b180d
Kelsea Fields <kelsea.fields@yahoo.com>
2021-01-03 16:45:40 -0800

    fixing up server

--- server.js
+++ server.js
@@ -16,10 +16,11 @@ app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 var routes = require("./controllers/burgers_controllers");
+// const orm = require("./config/orm");
 
 app.use(routes);
 
-app.listen(PORT, function() {
-    // Log (server-side) when our server has started
-    console.log("Server listening on: http://localhost:" + PORT);
-  });
+app.listen(PORT, function () {
+  // Log (server-side) when our server has started
+  console.log("Server listening on: http://localhost:" + PORT);
+});
