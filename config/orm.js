07eae4f7f0519e44eb738f164f58ebb6f8f70648
Kelsea Fields <kelsea.fields@yahoo.com>
2021-01-05 11:53:24 -0800

    adding delete to orm

--- config/orm.js
+++ config/orm.js
@@ -75,6 +75,20 @@ var orm = {
       cb(result);
     });
   },
+
+  delete: function (table, condition, cd) {
+    var queryString = "DELETE FROM " + table;
+    queryString += " WHERE ";
+    queryString += condition;
+
+    connection.query(queryString, function (err, result) {
+      if (err) {
+        throw err;
+      }
+
+      cb(result);
+    });
+  },
 };
 
 module.exports = orm;
